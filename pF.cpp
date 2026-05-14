b1133ec3b2529dd864c5d3ee7dd69f96bc037482
Alex Droblas <adroblas@mail.utdt.edu>
2026-05-14 11:22:17 -0300

    gitignore

--- pF.cpp
+++ pF.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 
 using namespace std; 
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
 
     return 0;
 }
