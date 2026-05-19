41bf41290b40ea3029e2f1465de7df3daeccce73
AlexDro71 <alexdro611@gmail.com>
2026-05-19 18:11:37 +0000

    Avanzando en C y H

--- pC2025.cpp
+++ pC2025.cpp
@@ -2,9 +2,39 @@
 
 using namespace std; 
 
+int s(const vector<int> &p){
+    int res=0;
+    for(int i=0; i<p.size(); i++){
+        if(p[p[i]]==i) res+=1;
+    }
+    return res;
+}
+
+void rot(vector<int> &p){
+    for(int i=0; i<p.size()-1; i++){
+        swap(p[i], p[i+1]);
+    }
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> p;
+    for(int i=0; i<n; i++){
+        int pn;
+        cin >> pn;
+        p.push_back(pn-1);
+    }
+    int res=s(p);
+    for(int i=1; i<p.size(); i++){
+        rot(p);
+        res+=s(p);
+    }
 
+
+
+    cout << res << "\n";
     return 0;
 }
--- pH2025.cpp
+++ pH2025.cpp
@@ -5,6 +5,24 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    long long n;
+    cin >> n;
+    vector<long long> divs;
+    for(int i=0; i<n; i++){
+        long long d;
+        cin >> d;
+        divs.push_back(d);
+    }
+    bool posible = false;
+    long long x;
+    long long dn;
 
+
+    
+    if(posible){
+        cout << x << dn << "\n";
+    }else{
+        cout << '*' << "\n";
+    }
     return 0;
 }
